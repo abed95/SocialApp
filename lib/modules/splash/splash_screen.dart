80e4008df7ce169f78a090a8e3c78bdd22cbbe65
abed95 <d.abedo95@gmail.com>
2024-12-10 11:58:17 +0200

    - update add note

--- lib/modules/splash/splash_screen.dart
+++ lib/modules/splash/splash_screen.dart
@@ -19,6 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
     Future.delayed(const Duration(seconds: 3), () {
       String? userID;
+      //test
       CacheHelper.getUserData().then((onValue) {
         userID = onValue!.userID;
         print('Splash 25 : $userID');
